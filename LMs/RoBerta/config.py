a868e043bcc287704ac89795038506068adf1d14
sktsherlock <1033270581@qq.com>
2022-11-18 18:48:56 +0800

    [HAO] preprocess.py

--- LMs/RoBerta/config.py
+++ LMs/RoBerta/config.py
@@ -42,7 +42,7 @@ class RoBERTaConfig(LMConfig):
                 hf_model='roberta-large',
                 hidden_dim=1024,
                 max_bsz=SN(  # Batch size for different device
-                    train={12: 6, 16: 10, 24: 16, 32: 10},
+                    train={12: 6, 16: 10, 24: 16, 32: 12},
                     inf={12: 150, 16: 200, 24: 150, 32: 250},
                 ),
                 prt_lm={  # Initial LM configs
