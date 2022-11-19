972420bc667f42fef1a2cfa963df1ca98c282036
sktsherlock <1033270581@qq.com>
2022-11-20 05:00:15 +0800

    [HAO] trainLM

--- LMs/RoBerta/config.py
+++ LMs/RoBerta/config.py
@@ -15,6 +15,7 @@ class RoBERTaConfig(LMConfig):
             SN(
                 hf_model='roberta-base',
                 hidden_dim=768,
+                father_model='RoBerta',
                 max_bsz=SN(  # Batch size for different device
                     train={12: 8, 16: 12, 24: 9, 32: 30, 40: 18, 70: 48},
                     inf={12: 150, 16: 200, 24: 150, 32: 512, 40: 300, 70: 560},
@@ -40,6 +41,7 @@ class RoBERTaConfig(LMConfig):
         'Roberta-large':
             SN(
                 hf_model='roberta-large',
+                father_model = 'RoBerta',
                 hidden_dim=1024,
                 max_bsz=SN(  # Batch size for different device
                     train={12: 6, 16: 10, 24: 16, 32: 12},
