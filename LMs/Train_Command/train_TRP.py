06806978ba8c82b85599af2b57c3f97dcf319332
sktsherlock <1033270581@qq.com>
2022-12-06 15:50:42 +0800

    [HAO] train TRP

---
+++ LMs/Train_Command/train_TRP.py
@@ -0,0 +1,18 @@
+import os.path as osp
+import sys
+
+# sys.path.append((osp.abspath(osp.dirname(__file__)).split('src')[0] + 'src'))
+from .lm_utils import *
+
+if __name__ == "__main__":
+    # ! Init Arguments
+    model = get_lm_model()
+    Trainer = get_TRP_trainer(model)
+    Config = get_lm_config(model)
+
+    args = Config().parse_args()
+    cf = Config(args).init()
+    # ! Load data and train
+    trainer = Trainer(cf=cf)
+    trainer.train()
+    trainer.eval_and_save()
