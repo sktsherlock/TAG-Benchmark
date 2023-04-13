41c709ef84025ead2c88c2f3cdcf157308b2f2b6
sktsherlock <1033270581@qq.com>
2023-04-14 00:29:17 +0800

    [HAO] Benchmark

--- LMs/Train_Command/Pretrain/TDeepWalk/Tinybert.py
+++ LMs/Train_Command/Pretrain/TDeepWalk/Tinybert.py
@@ -1,5 +1,5 @@
-"""
-#!命令
+#!不同模型对应的命令
+""" TinyBert
 CUDA_VISIBLE_DEVICES=0 /usr/bin/env python sweep/dist_runner.py LMs/Train_Command/train_DPK.py --PrtMode=Deepwalk \
 --att_dropout=0.1 --cla_dropout=0.1 --dataset=arxiv_TA --dropout=0.1 --epochs=5 --eq_batch_size=30 --eval_patience=50000 --label_smoothing_factor=0.1 --lr=2e-05 --model=Deberta --warmup_epochs=1 --gpus=0
 
