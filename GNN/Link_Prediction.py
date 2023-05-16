fe938c77bc25dcb19e7ae89096d6472ece2eb20b
sktsherlock <1033270581@qq.com>
2023-05-16 11:29:07 +0800

    [HAO] Link Prediction Baseline

--- GNN/Link_Prediction.py
+++ GNN/Link_Prediction.py
@@ -1,139 +1,195 @@
-#%%
-import itertools
-import os
-from sklearn.metrics import roc_auc_score
-os.environ["DGLBACKEND"] = "pytorch"
-from dgl.nn import SAGEConv
-from model.GNN_library import GraphSAGE
-import numpy as np
-import scipy.sparse as sp
+import argparse
+
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-#%%
-import dgl
+from torch.utils.data import DataLoader
 
-g = dgl.load_graphs("/mnt/v-wzhuang/Amazon/Books/Amazon-Books-History.pt")[0][0]
-g = dgl.to_bidirected(g)
-#%%
-# Split edge set for training and testing
-u, v = g.edges()
-np.random.seed(42)
-eids = np.arange(g.num_edges())
-eids = np.random.permutation(eids) # 打乱边顺序
+from ogb.linkproppred import PygLinkPropPredDataset, Evaluator
 
-test_size = int(len(eids) * 0.2)
-train_size = g.num_edges() - test_size
-test_pos_u, test_pos_v = u[eids[:test_size]], v[eids[:test_size]]
-train_pos_u, train_pos_v = u[eids[test_size:]], v[eids[test_size:]]
+class LinkPredictor(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
+                 dropout):
+        super(LinkPredictor, self).__init__()
 
-# Find all negative edges and split them for training and testing
-adj = sp.coo_matrix((np.ones(len(u)), (u.numpy(), v.numpy())))
-adj_neg = 1 - adj.todense() - np.eye(g.num_nodes())
-neg_u, neg_v = np.where(adj_neg != 0)
+        self.lins = torch.nn.ModuleList()
+        self.lins.append(torch.nn.Linear(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.lins.append(torch.nn.Linear(hidden_channels, hidden_channels))
+        self.lins.append(torch.nn.Linear(hidden_channels, out_channels))
 
-#! 选择10000个作为负样本
-neg_eids = np.random.choice(len(neg_u), g.num_edges())
+        self.dropout = dropout
 
-test_neg_u, test_neg_v = (
-    neg_u[neg_eids[:test_size]],
-    neg_v[neg_eids[:test_size]],
-)
-train_neg_u, train_neg_v = (
-    neg_u[neg_eids[test_size:]],
-    neg_v[neg_eids[test_size:]],
-)
-#%%
-device = f'cuda' if torch.cuda.is_available() else 'cpu'
-device = torch.device(device)
+    def reset_parameters(self):
+        for lin in self.lins:
+            lin.reset_parameters()
 
-train_g = dgl.remove_edges(g, eids[:test_size]).to(device)
-#%%
+    def forward(self, x_i, x_j):
+        x = x_i * x_j
+        for lin in self.lins[:-1]:
+            x = lin(x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.lins[-1](x)
+        return torch.sigmoid(x)
 
+def train(predictor, x, split_edge, optimizer, batch_size):
+    predictor.train()
 
+    pos_train_edge = split_edge['train']['edge'].to(x.device)
 
-# ----------- 2. create model -------------- #
-# build a two-layer GraphSAGE model
+    total_loss = total_examples = 0
+    for perm in DataLoader(range(pos_train_edge.size(0)), batch_size,
+                           shuffle=True):
+        optimizer.zero_grad()
 
-#%%
-train_pos_g = dgl.graph((train_pos_u, train_pos_v), num_nodes=g.num_nodes()).to(device)
-train_neg_g = dgl.graph((train_neg_u, train_neg_v), num_nodes=g.num_nodes()).to(device)
+        edge = pos_train_edge[perm].t()
 
-test_pos_g = dgl.graph((test_pos_u, test_pos_v), num_nodes=g.num_nodes()).to(device)
-test_neg_g = dgl.graph((test_neg_u, test_neg_v), num_nodes=g.num_nodes()).to(device)
-#%%
-import dgl.function as fn
+        pos_out = predictor(x[edge[0]], x[edge[1]])
+        pos_loss = -torch.log(pos_out + 1e-15).mean()
 
+        # Just do some trivial random sampling.
+        edge = torch.randint(0, x.size(0), edge.size(), dtype=torch.long,
+                             device=x.device)
+        neg_out = predictor(x[edge[0]], x[edge[1]])
+        neg_loss = -torch.log(1 - neg_out + 1e-15).mean()
 
-class DotPredictor(nn.Module):
-    def forward(self, g, h):
-        with g.local_scope():
-            g.ndata["h"] = h
-            # Compute a new edge feature named 'score' by a dot-product between the
-            # source node feature 'h' and destination node feature 'h'.
-            g.apply_edges(fn.u_dot_v("h", "h", "score"))
-            # u_dot_v returns a 1-element vector for each edge so you need to squeeze it.
-            return g.edata["score"][:, 0]
-#%%
-feat = torch.from_numpy(np.load('/mnt/v-wzhuang/TAG/Finetune/Amazon/History/TinyBert/emb.npy').astype(np.float32)).to(device)
-in_feats = feat.shape[1]
+        loss = pos_loss + neg_loss
+        loss.backward()
+        optimizer.step()
 
-model = GraphSAGE(in_feats, 16, n_classes=16, n_layers=2, activation= F.relu,
-                  dropout=0.2, aggregator_type='mean').to(device)
-# You can replace DotPredictor with MLPPredictor.
-# pred = MLPPredictor(16)
-pred = DotPredictor().to(device)
+        num_examples = pos_out.size(0)
+        total_loss += loss.item() * num_examples
+        total_examples += num_examples
+
+    return total_loss / total_examples
 
 
-def compute_loss(pos_score, neg_score):
-    scores = torch.cat([pos_score, neg_score])
-    labels = torch.cat(
-        [torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])]
-    ).to(device)
-    return F.binary_cross_entropy_with_logits(scores, labels)
+@torch.no_grad()
+def test(predictor, x, split_edge, evaluator, batch_size):
+    predictor.eval()
+
+    pos_train_edge = split_edge['train']['edge'].to(x.device)
+    pos_valid_edge = split_edge['valid']['edge'].to(x.device)
+    neg_valid_edge = split_edge['valid']['edge_neg'].to(x.device)
+    pos_test_edge = split_edge['test']['edge'].to(x.device)
+    neg_test_edge = split_edge['test']['edge_neg'].to(x.device)
+
+    pos_train_preds = []
+    for perm in DataLoader(range(pos_train_edge.size(0)), batch_size):
+        edge = pos_train_edge[perm].t()
+        pos_train_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+    pos_train_pred = torch.cat(pos_train_preds, dim=0)
+
+    pos_valid_preds = []
+    for perm in DataLoader(range(pos_valid_edge.size(0)), batch_size):
+        edge = pos_valid_edge[perm].t()
+        pos_valid_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+    pos_valid_pred = torch.cat(pos_valid_preds, dim=0)
+
+    neg_valid_preds = []
+    for perm in DataLoader(range(neg_valid_edge.size(0)), batch_size):
+        edge = neg_valid_edge[perm].t()
+        neg_valid_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+    neg_valid_pred = torch.cat(neg_valid_preds, dim=0)
+
+    pos_test_preds = []
+    for perm in DataLoader(range(pos_test_edge.size(0)), batch_size):
+        edge = pos_test_edge[perm].t()
+        pos_test_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+    pos_test_pred = torch.cat(pos_test_preds, dim=0)
+
+    neg_test_preds = []
+    for perm in DataLoader(range(neg_test_edge.size(0)), batch_size):
+        edge = neg_test_edge[perm].t()
+        neg_test_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+    neg_test_pred = torch.cat(neg_test_preds, dim=0)
+
+    results = {}
+    for K in [10, 50, 100]:
+        evaluator.K = K
+        train_hits = evaluator.eval({
+            'y_pred_pos': pos_train_pred,
+            'y_pred_neg': neg_valid_pred,
+        })[f'hits@{K}']
+        valid_hits = evaluator.eval({
+            'y_pred_pos': pos_valid_pred,
+            'y_pred_neg': neg_valid_pred,
+        })[f'hits@{K}']
+        test_hits = evaluator.eval({
+            'y_pred_pos': pos_test_pred,
+            'y_pred_neg': neg_test_pred,
+        })[f'hits@{K}']
+
+        results[f'Hits@{K}'] = (train_hits, valid_hits, test_hits)
+
+    return results
 
 
-def compute_auc(pos_score, neg_score):
-    scores = torch.cat([pos_score, neg_score]).cpu().numpy()
-    labels = torch.cat(
-        [torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])]
-    ).cpu().numpy()
-    return roc_auc_score(labels, scores)
-#%%
-# ----------- 3. set up loss and optimizer -------------- #
-# in this case, loss will in training loop
-optimizer = torch.optim.Adam(
-    itertools.chain(model.parameters(), pred.parameters()), lr=0.01
-)
+def main():
+    parser = argparse.ArgumentParser(description='OGBL-COLLAB (MLP)')
+    parser.add_argument('--device', type=int, default=0)
+    parser.add_argument('--log_steps', type=int, default=1)
+    parser.add_argument('--use_node_embedding', action='store_true')
+    parser.add_argument('--num_layers', type=int, default=3)
+    parser.add_argument('--hidden_channels', type=int, default=256)
+    parser.add_argument('--dropout', type=float, default=0.0)
+    parser.add_argument('--batch_size', type=int, default=64 * 1024)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--epochs', type=int, default=200)
+    parser.add_argument('--eval_steps', type=int, default=1)
+    parser.add_argument('--runs', type=int, default=10)
+    args = parser.parse_args()
+    print(args)
 
-with torch.no_grad():
-    h = model(train_g, feat).to(device)
-    pos_score = pred(test_pos_g, h)
-    neg_score = pred(test_neg_g, h)
-    print("AUC", compute_auc(pos_score, neg_score))
+    device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device)
 
-# ----------- 4. training -------------------------------- #
-all_logits = []
-for e in range(100):
-    # forward
-    h = model(train_g, feat).to(device)
-    pos_score = pred(train_pos_g, h)
-    neg_score = pred(train_neg_g, h)
-    loss = compute_loss(pos_score, neg_score)
+    dataset = PygLinkPropPredDataset(name='ogbl-collab')
+    split_edge = dataset.get_edge_split()
+    data = dataset[0]
 
-    # backward
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    x = data.x
+    if args.use_node_embedding:
+        embedding = torch.load('embedding.pt', map_location='cpu')
+        x = torch.cat([x, embedding], dim=-1)
+    x = x.to(device)
 
-    if e % 5 == 0:
-        print("In epoch {}, loss: {}".format(e, loss))
+    predictor = LinkPredictor(x.size(-1), args.hidden_channels, 1,
+                              args.num_layers, args.dropout).to(device)
 
-# ----------- 5. check results ------------------------ #
+    evaluator = Evaluator(name='ogbl-collab')
+
+    # loggers = {
+    #     'Hits@10': Logger(args.runs, args),
+    #     'Hits@50': Logger(args.runs, args),
+    #     'Hits@100': Logger(args.runs, args),
+    # }
+
+    for run in range(args.runs):
+        predictor.reset_parameters()
+        optimizer = torch.optim.Adam(predictor.parameters(), lr=args.lr)
+
+        for epoch in range(1, 1 + args.epochs):
+            loss = train(predictor, x, split_edge, optimizer, args.batch_size)
+
+            if epoch % args.eval_steps == 0:
+                results = test(predictor, x, split_edge, evaluator,
+                               args.batch_size)
 
 
-with torch.no_grad():
-    pos_score = pred(test_pos_g, h)
-    neg_score = pred(test_neg_g, h)
-    print("AUC", compute_auc(pos_score, neg_score))
+                if epoch % args.log_steps == 0:
+                    for key, result in results.items():
+                        train_hits, valid_hits, test_hits = result
+                        print(key)
+                        print(f'Run: {run + 1:02d}, '
+                              f'Epoch: {epoch:02d}, '
+                              f'Loss: {loss:.4f}, '
+                              f'Train: {100 * train_hits:.2f}%, '
+                              f'Valid: {100 * valid_hits:.2f}%, '
+                              f'Test: {100 * test_hits:.2f}%')
+                    print('---')
 
+
+
+if __name__ == "__main__":
+    main()
