329ee661cb20ae723bd6e341341342f5e7fb889e
sktsherlock <1033270581@qq.com>
2023-09-13 09:30:54 +0800

    Version_1.20

--- LMs/Train_Command/Inference/deepwalk.py
+++ LMs/Train_Command/Inference/deepwalk.py
@@ -7,14 +7,16 @@ from gensim.models import Word2Vec
 import networkx as nx
 from karateclub import DeepWalk
 
-print("ok")
-# root_dir = os.path.abspath(os.path.expanduser('/mnt/v-wzhuang/TAG-Benchmark/data/ogb/'))
-# dataset = DglNodePropPredDataset('ogbn-arxiv', root=root_dir)
-g = dgl.load_graphs('/mnt/v-wzhuang/TAG-Benchmark/data/amazon/Sports/Fit/Sports-Fitness.pt')[0][0]
+# Load graphs
+g = dgl.load_graphs('TAG-Benchmark/data/amazon/Sports/Fit/Sports-Fitness.pt')[0][0]
 g = dgl.to_bidirected(g)
 nx_G = g.to_networkx()
-model = DeepWalk(walk_number=10,walk_length=80,dimensions=128,workers=4,window_size=5,epochs=1,learning_rate=0.05,min_count=1,seed=42)  # node embedding algorithm
+
+# Deepwalk in the graph and then get the node embeddings with high topology information
+model = DeepWalk(walk_number=10, walk_length=80, dimensions=128,workers=4, window_size=5, epochs=1,learning_rate=0.05, min_count=1,seed=42)  # node embedding algorithm
+
 model.fit(nx_G)  # fit it on the graph
 embedding = model.get_embedding()  # extract embeddings
-np.save('/mnt/v-wzhuang/TAG-Benchmark/data/amazon/Sports/Fit/deepwalk_feat.npy', embedding)
+
+np.save('TAG-Benchmark/data/amazon/Sports/Fit/deepwalk_feat.npy', embedding)
 print("finish")
