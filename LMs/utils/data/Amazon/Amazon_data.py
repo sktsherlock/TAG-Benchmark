9d44556deacd71042d3b32e6be20cf3898e6a8cd
sktsherlock <1033270581@qq.com>
2024-03-27 17:15:30 +0800

    TAG 1.5

--- LMs/utils/data/Amazon/Amazon_data.py
+++ LMs/utils/data/Amazon/Amazon_data.py
@@ -8,18 +8,19 @@ from tqdm import tqdm
 from utils.function.os_utils import mkdir_p
 
 def _tokenize_amazon_datasets(d):
-    #! 创建目录
-    if not osp.exists(osp.join(d.data_root, f'{d.data_name}.txt')):
+    if not osp.exists(osp.join(d.data_root, f'{d.data_name}.csv')):
         mkdir_p(d.data_root)
-        raise{f'Please input the txt to the {d.data_root}'}
+        print(f'Please check there is a file in the {d.data_root}')
     #! Tokenize the data
     else:
-        text = pd.read_csv(osp.join(d.data_root, f'{d.data_name}.txt'), header=None, sep='\t')
-        text = text[0]
+        df = pd.read_csv(osp.join(d.data_root, f'{d.data_name}.csv'))
+        text = df['text'].tolist()
+        # text = pd.read_csv(osp.join(d.data_root, f'{d.data_name}.txt'), header=None, sep='\t')
+        # text = text[0]
         # Look at
     #! For debug
     tokenizer = AutoTokenizer.from_pretrained(d.hf_model)
-    tokenized = tokenizer(text.tolist(), padding='max_length', truncation=True, max_length=d.max_length,
+    tokenized = tokenizer(text, padding='max_length', truncation=True, max_length=d.max_length,
                           return_token_type_ids=True).data
     mkdir_p(d._token_folder)
     for k in tokenized:
